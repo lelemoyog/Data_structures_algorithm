5e11b1f58680cf2c4fd9270487eb02a54df07c5e
saitemu_issa <saitemu.issa@student.moringaschool.com>
2024-01-28 23:17:55 +0300

    Done with Quiz one

--- Quiz One /Qfour.cpp
+++ Quiz One /Qfour.cpp
@@ -7,7 +7,7 @@ int singleNumber(std::vector<int>& nums) {
 }
 
 int main() {
-    std::vector<int> nums = {2, 1, 4, 1, 2};  // Replace with your input
+    std::vector<int> nums = {2, 1, 4, 1, 2};  // input
     std::cout << "The single number is " << singleNumber(nums) << std::endl;
     return 0;
 }
