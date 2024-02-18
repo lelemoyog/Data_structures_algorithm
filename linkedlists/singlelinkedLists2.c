16e61f3a134afbdc0e9b7d1bdc854fb5b38decd9
Lelemoyog <issasaitemu@gmail.com>
2024-02-18 11:27:54 +0300

    linkedlist

---
+++ linkedlists/singlelinkedLists2.c
@@ -0,0 +1,52 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct node{
+    int data;
+    struct node *link;
+};
+
+
+void count_of_nodes(struct node *head) {
+int count = 0;
+if(head == NULL)
+printf("Linked List is empty");
+struct node *ptr = NULL;
+ptr = head;
+while(ptr != NULL) {
+count++;
+ptr = ptr->link;
+}
+printf("%d", count);
+}
+
+
+int main() {
+    // Write C code here
+    struct node *head = malloc(sizeof(struct node));
+    head->data = 98;
+    head->link = NULL;
+    
+    struct node *current = malloc(sizeof(struct node));
+    current->data = 45;
+    current->link = NULL;
+    head->link = current;
+    
+    
+    current = malloc(sizeof(struct node));
+
+    current->data = 3;
+    current->link = NULL;
+    head->link->link = current;
+    
+    //0x561fb308f2e0
+    
+    
+   // printf("%p", current);
+
+    count_of_nodes(head);
+
+    return 0;
+};
